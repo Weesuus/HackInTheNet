399b87f5399f8db190deecdf34834aab69b8e2e0
Weesus <70699787+Weesuus@users.noreply.github.com>
2022-12-08 00:55:19 +0100

    updates 08/12/22
    -Added ls(as example)

--- index.js
+++ index.js
@@ -15,36 +15,50 @@ let diskTest = {
 	}, "bin": {}, "log":{}, "sys":{"osT.sys": "version:X\ntheme:sus"}
 };
 
-let nowPath = "/home/USERNAME/";
-let nowDisp = "[USERNAME@MACHINE FOLDER] ~ "; //HOME ~ - ANY OTHER $
+let nowPath = "/home/weesus/";
+let nowDisp = "[weesus@MACHINE] ~ "; //HOME ~ - ANY OTHER $
 
 txtpre.innerText = nowDisp;
 
 function recognizeCommand(){
-	if(txt1.value.startsWith('help')){
+	if(txt1.value.startsWith('help ') || txt1.value.startsWith('help')){
 		visualizer();
 		containerMain.innerHTML += '<p class="no-off">' + "help - Visualize all the Commands" + '</p>';
 		containerMain.innerHTML += '<p class="no-off">' + "echo - Prints a String" + '</p>';
 		containerMain.innerHTML += '<p class="no-off">' + "clear - Clear the Console" + '</p>';
 		containerMain.innerHTML += '<p class="no-off">' + "ls - List Files and Directory in Current Path or Given Path" + '</p>';
-		txt1.value = ""
-	}else if(txt1.value.startsWith('echo')){
+		txt1.value = "";
+	}else if(txt1.value.startsWith('echo ')){
 		visualizer(txt1.value);
 		containerMain.innerHTML += '<p class="no-off">' + txt1.value.slice(5) + '</p>';
-		txt1.value = ""
+		txt1.value = "";
 	}else if(txt1.value.startsWith('clear')){
-		containerMain.innerHTML = ""
-		txt1.value = ""
+		containerMain.innerHTML = "";
+		txt1.value = "";
 	}else if(txt1.value.startsWith('ls')){
-		if(txt1.value.slice(3) != ''){
+		if(txt1.value.startsWith('ls ')){
 			//PASSED
+			visualizer(txt1.value);
+			// console.log(txt1.value.slice(3));
+			let tmpF = listFiles(txt1.value.slice(3));
+			// tmpF.forEach(element => containerMain.innerHTML += '<p class="no-off">' + element + '</p>');
+			Object.entries(tmpF).forEach(([key, value]) => {
+				containerMain.innerHTML += '<p class="no-off">' + key + '</p>';
+			})
+			txt1.value = "";
 		}else{
 			//NOT PASSED
+			visualizer(txt1.value);
+			let tmpF = listFiles(nowPath);
+			Object.entries(tmpF).forEach(([key, value]) => {
+				containerMain.innerHTML += '<p class="no-off">' + key + '</p>';
+			})
+			txt1.value = "";
 		}
 	}else{
 		visualizer();
 		containerMain.innerHTML += '<p class="no-off">' + txt1.value + " - Unknown Command" + '</p>';
-		txt1.value = ""
+		txt1.value = "";
 	}
 }
 
@@ -55,7 +69,35 @@ function visualizer(){
 }
 
 function listFiles(passed){
-	
+	let lastX = "", counterX = 0, lastStart = 0, lastObj = null;
+	for (var i = 0; i <= passed.length - 1; i++) {
+		// console.log(i)
+		// console.log(passed.length)
+		// console.log(passed[i])
+
+		if(passed[i] == '/'){
+			if(counterX == 0){
+				// console.log(22)
+				lastObj = diskTest;
+			}else if(i == (passed.length - 1)){
+				lastObj = lastObj[lastX];
+				return lastObj;
+				// console.log("lastObj");
+            	break;
+        	}else{
+				lastObj = lastObj[lastX];
+			// console.log(lastObj);
+				lastStart = counterX;
+				lastX = "";
+			}
+		}else{
+        	// console.log(passed.length)
+        	// console.log(i)
+			lastX += passed[i];
+		}
+		counterX+=1;
+	}
+	// console.log(lastObj)
 }
 
 txt1.addEventListener("keypress", function(event) {
